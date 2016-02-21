d845af63be0c988b3710567488704688cf096e04
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2016-02-21 17:00:26 -0600

    use show excel action

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -16,20 +16,20 @@ angular.module('webApp')
     ];
     
     $scope.placeTitle = "Place A";
-    $scope.excelUrl = "https://docs.google.com/spreadsheets/d/1FQLeGffcQq9ipQdRx7VA_ZeaW4l7FnHic384dhxcw2M/edit?usp=sharing"
+    $scope.excelId = CONFIG.Excel_PlaceA;
 
     $rootScope.$on('selectLand', function(event, data) {
       var newExtent = null;
       if (data == 'beta') {
       	$scope.placeTitle = "Place B";
-      	$scope.excelUrl = "https://docs.google.com/spreadsheets/d/1wwP1qVpfAHNuB9OTNKdEkxJtORdKPmlVcRtV3cVNI6I/edit?usp=sharing"
+      	$scope.excelId = CONFIG.Excel_PlaceB;
         newExtent = new esri.geometry.Extent(3318432.6374165327, 8341772.985917651, 3322951.9767140127, 8343683.911624831,
           new esri.SpatialReference({
             "wkid": 102113
           }));
       } else {
       	$scope.placeTitle = "Place A";
-      	$scope.excelUrl = "https://docs.google.com/spreadsheets/d/1FQLeGffcQq9ipQdRx7VA_ZeaW4l7FnHic384dhxcw2M/edit?usp=sharing"
+      	$scope.excelId = CONFIG.Excel_PlaceA;
         newExtent = new esri.geometry.Extent(3320427.1661233706, 8343996.825709338, 3322686.835772111, 8344952.288562928,
           new esri.SpatialReference({
             "wkid": 102113
@@ -54,7 +54,7 @@ angular.module('webApp')
       clearHighlightObject();
       $http({
         method: 'GET',
-        url: CONFIG.ExcelAPI
+        url: CONFIG.ExcelAPI.format($scope.excelId)
       }).
       success(function(data, status) {
 		var filtered = data.filter(isSold);
@@ -75,7 +75,7 @@ angular.module('webApp')
       clearHighlightObject();
       $http({
         method: 'GET',
-        url: CONFIG.ExcelAPI
+        url: CONFIG.ExcelAPI.format($scope.excelId)
       }).
       success(function(data, status) {
 		var filtered = data.filter(isReserved);
--- web/app/scripts/services/config.js
+++ web/app/scripts/services/config.js
@@ -11,5 +11,7 @@ angular.module('webApp')
   .constant('CONFIG', {
     // AngularJS will instantiate a singleton by calling "new" on this function
     'GetGKN': "http://maps.rosreestr.ru/arcgis/rest/services/Cadastre/CadastreSelected/MapServer/exts/GKNServiceExtension/online/parcel/find?cadNums=['{0}']&onlyAttributes=false&returnGeometry=true&f=json",
-    'ExcelAPI': "http://169.55.85.43:3000/excel" //todo: fix hardcoded ip
+    'ExcelAPI': "http://169.55.85.43:3000/excel/{0}", //todo: fix hardcoded ip
+    'Excel_PlaceA': "1FQLeGffcQq9ipQdRx7VA_ZeaW4l7FnHic384dhxcw2M",
+    'Excel_PlaceB': "1wwP1qVpfAHNuB9OTNKdEkxJtORdKPmlVcRtV3cVNI6I"
   });
